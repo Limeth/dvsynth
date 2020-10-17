89897c1fe7dedef5fd1a9fd81d79888bf3bed4ca
Jakub Hlusička <limeth@protonmail.com>
2020-10-17 22:13:25 +0200

    Add a title to FloatingPane

--- src/widgets/floating_panes.rs
+++ src/widgets/floating_panes.rs
@@ -1,22 +1,31 @@
 use std::hash::Hash;
-use iced_native::{self, Size, Length, Point, Hasher, Event, Clipboard};
+use iced_native::{self, Size, Length, Point, Hasher, Event, Clipboard, Column, Text};
 use iced_native::{mouse, overlay, Element};
-use iced_native::widget::Widget;
+use iced_native::widget::{Widget, Container};
 use iced_native::layout::{Layout, Limits, Node};
 use iced_graphics::{self, Backend, Defaults, Primitive};
+use super::*;
 
-pub struct FloatingPane<'a, M: 'a, R: WidgetRenderer> {
+pub struct FloatingPaneBuilder<'a, M: 'a, R: 'a + WidgetRenderer> {
     pub element: Element<'a, M, R>,
     pub position: [i32; 2],
     pub title: Option<&'a str>,
+    pub title_style: Option<<R as iced_native::widget::container::Renderer>::Style>,
+    pub title_size: Option<u16>,
+    pub title_margin: Spacing,
+    pub pane_style: Option<<R as iced_native::widget::container::Renderer>::Style>,
 }
 
-impl<'a, M: 'a, R: 'a + WidgetRenderer> FloatingPane<'a, M, R> {
+impl<'a, M: 'a, R: 'a + WidgetRenderer> FloatingPaneBuilder<'a, M, R> {
     pub fn new(element: impl Into<Element<'a, M, R>>) -> Self {
         Self {
             element: element.into(),
             position: Default::default(),
             title: Default::default(),
+            title_style: Default::default(),
+            title_size: Default::default(),
+            title_margin: Default::default(),
+            pane_style: Default::default(),
         }
     }
 
@@ -28,6 +37,73 @@ impl<'a, M: 'a, R: 'a + WidgetRenderer> FloatingPane<'a, M, R> {
     pub fn title(mut self, title: Option<&'a str>) -> Self {
         self.title = title;
         self
+    }
+
+    pub fn title_style<T>(mut self, title_style: Option<T>) -> Self
+            where T: Into<<R as iced_native::widget::container::Renderer>::Style> {
+        self.title_style = title_style.map(Into::into);
+        self
+    }
+
+    pub fn title_size(mut self, title_size: Option<u16>) -> Self {
+        self.title_size = title_size;
+        self
+    }
+
+    pub fn title_margin(mut self, title_margin: Spacing) -> Self {
+        self.title_margin = title_margin;
+        self
+    }
+
+    pub fn pane_style<T>(mut self, pane_style: Option<T>) -> Self
+            where T: Into<<R as iced_native::widget::container::Renderer>::Style> {
+        self.pane_style = pane_style.map(Into::into);
+        self
+    }
+
+    pub fn build(mut self) -> FloatingPane<'a, M, R> {
+        FloatingPane {
+            element_tree: {
+                let mut column = Column::<M, R>::new();
+
+                if let Some(title) = self.title.take() {
+                    let mut text = Text::new(title.to_string());
+
+                    if let Some(title_size) = self.title_size.take() {
+                        text = text.size(title_size);
+                    }
+
+                    let margin = Margin::new(text, self.title_margin.clone());
+                    let mut container = Container::new(margin);
+
+                    if let Some(title_style) = self.title_style.take() {
+                        container = container.style(title_style);
+                    }
+
+                    column = column.push(container);
+                }
+
+                let mut container = Container::new(column.push(self.element));
+
+                if let Some(pane_style) = self.pane_style.take() {
+                    container = container.style(pane_style);
+                }
+
+                container.into()
+            },
+            position: self.position,
+        }
+    }
+}
+
+pub struct FloatingPane<'a, M: 'a, R: 'a + WidgetRenderer> {
+    pub element_tree: Element<'a, M, R>,
+    pub position: [i32; 2],
+}
+
+impl<'a, M: 'a, R: 'a + WidgetRenderer> FloatingPane<'a, M, R> {
+    pub fn builder(element: impl Into<Element<'a, M, R>>) -> FloatingPaneBuilder<'a, M, R> {
+        FloatingPaneBuilder::new(element)
     }
 }
 
@@ -112,7 +188,7 @@ impl<'a, M: 'a, R: 'a + WidgetRenderer> Widget<M, R> for FloatingPanes<'a, M, R>
             Size::new(self.max_width as f32, self.max_height as f32), // FIXME
             self.children.iter()
                 .map(|child| {
-                    let mut node = child.element.layout(renderer, &limits);
+                    let mut node = child.element_tree.layout(renderer, &limits);
 
                     node.move_to(Point::new(
                         child.position[0] as f32,
@@ -145,7 +221,7 @@ impl<'a, M: 'a, R: 'a + WidgetRenderer> Widget<M, R> for FloatingPanes<'a, M, R>
         self.max_height.hash(state);
 
         for child in &self.children {
-            child.element.hash_layout(state);
+            child.element_tree.hash_layout(state);
             child.position.hash(state);
         }
     }
@@ -161,7 +237,7 @@ impl<'a, M: 'a, R: 'a + WidgetRenderer> Widget<M, R> for FloatingPanes<'a, M, R>
     ) {
         self.children.iter_mut().zip(layout.children()).for_each(
             |(child, layout)| {
-                child.element.on_event(
+                child.element_tree.on_event(
                     event.clone(),
                     layout,
                     cursor_position,
@@ -180,7 +256,7 @@ impl<'a, M: 'a, R: 'a + WidgetRenderer> Widget<M, R> for FloatingPanes<'a, M, R>
         self.children
             .iter_mut()
             .zip(layout.children())
-            .filter_map(|(child, layout)| child.element.overlay(layout))
+            .filter_map(|(child, layout)| child.element_tree.overlay(layout))
             .next()
     }
 }
@@ -193,7 +269,13 @@ impl<'a, M: 'a, R: 'a + WidgetRenderer> From<FloatingPanes<'a, M, R>> for Elemen
 
 /// Good practice: Rendering is made to be generic over the backend using this trait, which
 /// is to be implemented on the specific `Renderer`.
-pub trait WidgetRenderer: iced_native::Renderer + Sized {
+pub trait WidgetRenderer:
+        margin::WidgetRenderer
+      + iced_native::Renderer
+      + iced_native::text::Renderer
+      + iced_native::column::Renderer
+      + iced_native::widget::container::Renderer
+      + Sized {
     fn draw<M>(
         &mut self,
         defaults: &Self::Defaults,
@@ -205,7 +287,7 @@ pub trait WidgetRenderer: iced_native::Renderer + Sized {
 
 impl<B> WidgetRenderer for iced_graphics::Renderer<B>
 where
-    B: Backend,
+    B: Backend + iced_graphics::backend::Text,
 {
     fn draw<Message>(
         &mut self,
@@ -223,7 +305,7 @@ where
                     .zip(layout.children())
                     .map(|(child, layout)| {
                         let (primitive, new_mouse_interaction) =
-                            child.element.draw(self, defaults, layout, cursor_position);
+                            child.element_tree.draw(self, defaults, layout, cursor_position);
 
                         if new_mouse_interaction > mouse_interaction {
                             mouse_interaction = new_mouse_interaction;
