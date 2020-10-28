2cd539e5cde186038e2324916ea9dca7f495d4e6
Jakub Hlusička <limeth@protonmail.com>
2020-10-28 18:21:18 +0100

    Remove unused code from main.rs

--- src/main.rs
+++ src/main.rs
@@ -178,15 +178,9 @@ type Graph = StableGraph<
 >;
 
 struct ApplicationState {
-    text_input_state: text_input::State,
-    text_input_value: String,
-
     graph: Graph,
-
     floating_panes_state: FloatingPanesState,
     floating_panes_content_state: FloatingPanesContentState,
-    floating_pane_state_0: FloatingPaneState,
-    floating_pane_state_1: FloatingPaneState,
 }
 
 #[derive(Debug, Clone)]
@@ -201,58 +195,6 @@ pub enum Message {
     InsertConnection { connection: Connection },
 }
 
-macro_rules! margin {
-    {
-        element: $element:expr,
-        spacing: $spacing:expr$(,)?
-    } => {{
-        use iced::{Row, Column, Space, Length};
-        Column::new()
-            .push(Space::with_height(Length::Units($spacing.up)))
-            .push(
-                Row::new()
-                    .push(Space::with_width(Length::Units($spacing.left)))
-                    .push($element)
-                    .push(Space::with_width(Length::Units($spacing.right)))
-            )
-            .push(Space::with_height(Length::Units($spacing.down)))
-    }}
-}
-
-macro_rules! ui_field {
-    {
-        name: $name:expr,
-        state: $state:expr,
-        placeholder: $placeholder:expr,
-        value: $value:expr,
-        on_change: $on_change:expr,
-        theme: $theme:expr$(,)?
-    } => {{
-        iced::Container::new(
-            // Margin::new(
-            margin! {
-                element: iced::Row::new()
-                    .spacing(consts::SPACING_HORIZONTAL)
-                    .align_items(Align::Center)
-                    .push(
-                        iced::Text::new($name)
-                            .size(14)
-                    )
-                    .push(
-                        iced::TextInput::new($state, $placeholder, $value, $on_change)
-                            .size(14)
-                            // .width(Length::Shrink)
-                            .padding(consts::SPACING_VERTICAL)
-                            .style($theme)
-                    ),
-                spacing: consts::SPACING,
-            }
-            // )
-        )
-        .style($theme)
-    }}
-}
-
 impl Application for ApplicationState {
     type Executor = iced::executor::Default;
     type Message = Message;
@@ -261,8 +203,6 @@ impl Application for ApplicationState {
     fn new(_flags: ()) -> (Self, Command<Self::Message>) {
         (
             Self {
-                text_input_state: Default::default(),
-                text_input_value: Default::default(),
                 graph: {
                     let mut graph = Graph::new();
 
@@ -301,8 +241,6 @@ impl Application for ApplicationState {
                 },
                 floating_panes_state: Default::default(),
                 floating_panes_content_state: FloatingPanesContentState::default(),
-                floating_pane_state_0: FloatingPaneState::with_position([0.0, 0.0]),
-                floating_pane_state_1: FloatingPaneState::with_position([100.0, 100.0]),
             },
             Command::none(),
         )
@@ -380,62 +318,6 @@ impl Application for ApplicationState {
         }
 
         panes.into()
-
-        //         // We use a column: a simple vertical layout
-        //         iced::Element::new(
-        //             FloatingPanes::new(&mut self.floating_panes_state)
-        //                 .push(
-        //                     FloatingPane::builder(
-        //                         &mut self.floating_pane_state_0,
-        //                         Column::new()
-        //                             .width(Length::Units(256))
-        //                             .push(
-        //                                 ui_field! {
-        //                                     name: "Test Text Input",
-        //                                     state: &mut self.text_input_state,
-        //                                     placeholder: "Placeholder",
-        //                                     value: &self.text_input_value,
-        //                                     on_change: |new_value| {
-        //                                         Message::UpdateTextInput(new_value.to_string())
-        //                                     },
-        //                                     theme: theme,
-        //                                 }
-        //                             ),
-        //                     )
-        //                     .title(Some("First"))
-        //                     .title_size(Some(16))
-        //                     .title_margin(consts::SPACING)
-        //                     .pane_style(Some(theme))
-        //                     .build(),
-        //                 )
-        //                 .push(
-        //                     FloatingPane::builder(
-        //                         &mut self.floating_pane_state_1,
-        //                         Column::new()
-        //                             .width(Length::Units(256))
-        //                             .push(
-        //                                 iced::Container::new(
-        //                                     // Margin::new(
-        //                                     margin! {
-        //                                         element: iced::Container::new(
-        //                                             Text::new("Test Node - Node Type")
-        //                                                 .size(16)
-        //                                         ),
-        //                                         spacing: consts::SPACING,
-        //                                     }
-        //                                     // )
-        //                                 )
-        //                                 .width(Length::Fill)
-        //                                 .style(theme)
-        //                             ),
-        //                     )
-        //                     .title(Some("Second"))
-        //                     .title_size(Some(16))
-        //                     .title_margin(consts::SPACING)
-        //                     .pane_style(Some(theme))
-        //                     .build(),
-        //                 )
-        //         )
     }
 }
 
